8236fb807837872d04ecae9c79403797e8d02a7e
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2025-09-15 11:22:40 -0300

    Modelo Usuário

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-import datetime
-from django.utils import timezone
-
-
 class Livro(models.Model):
     titulo = models.CharField(max_length=200, null=False)
     autor = models.TextField(null=False)
@@ -15,3 +11,11 @@ class Livro(models.Model):
 
     def string_detalhada(self):
         return "id: " + str(self.id) + "; titulo: " + "; autor: " + self.autor + "; ano publicação: " + str(self.anoPublicacao) + "; isbn: " + str(self.isbn)
+
+class Usuario(models.Model):
+    nome = models.CharField(null=False)
+    email = models.EmailField((""), max_length=254, null=False)
+    dataCadastro = models.DateField(null=False)
+
+    def __str__(self):
+        return f"{self.nome} - {self.email} - {self.dataCadastro}"
