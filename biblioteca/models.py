8ff6a031c4ac77d8d7889202b4e221cadbe53ab0
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2025-09-15 11:16:39 -0300

    Modelo Livro

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+import datetime
+from django.utils import timezone
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200, null=False)
+    autor = models.TextField(null=False)
+    anoPublicacao = models.IntegerField(null=False)
+    isbn = models.TextField(null=False)
+
+    def __str__(self):
+        return "[" + str(self.id) + "] " + self.titulo
+
+    def string_detalhada(self):
+        return "id: " + str(self.id) + "; titulo: " + "; autor: " + self.autor + "; ano publicação: " + str(self.anoPublicacao) + "; isbn: " + str(self.isbn)
